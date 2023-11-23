f2dc43e56cfe2206d8a9d7964886d49db05424b3
Anupam Kumar <kyteinsky@gmail.com>
2023-11-23 16:51:58 +0530

    Add mimetype for ReStructuredText
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- lib/private/Repair/RepairMimeTypes.php
+++ lib/private/Repair/RepairMimeTypes.php
@@ -246,6 +246,14 @@ class RepairMimeTypes implements IRepairStep {
 		return $this->updateMimetypes($updatedMimetypes);
 	}
 
+	private function introduceReStructuredTextFormatType() {
+		$updatedMimetypes = [
+			'rst' => 'text/x-rst',
+		];
+
+		return $this->updateMimetypes($updatedMimetypes);
+	}
+
 	/**
 	 * Fix mime types
 	 */
@@ -309,6 +317,10 @@ class RepairMimeTypes implements IRepairStep {
 
 		if (version_compare($ocVersionFromBeforeUpdate, '29.0.0.2', '<') && $this->introduceEmlAndMsgFormatType()) {
 			$out->info('Fixed eml and msg mime type');
+		}
+
+		if (version_compare($ocVersionFromBeforeUpdate, '29.0.0.3', '<') && $this->introduceReStructuredTextFormatType()) {
+			$out->info('Fixed ReStructured Text mime type');
 		}
 	}
 }
