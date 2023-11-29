35b2bcbe6b926f5765457c074c6a48c8a1f83a84
Julius Härtl <jus@bitgrid.net>
2023-11-29 09:17:26 +0100

    fix: Add reconnect check in case of timeouts on the db side
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/private/DB/Connection.php
+++ lib/private/DB/Connection.php
@@ -41,6 +41,7 @@ use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\Connections\PrimaryReadReplicaConnection;
 use Doctrine\DBAL\Driver;
 use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Exception\ConnectionLost;
 use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Platforms\OraclePlatform;
 use Doctrine\DBAL\Platforms\SqlitePlatform;
@@ -78,6 +79,7 @@ class Connection extends PrimaryReadReplicaConnection {
 
 	/** @var DbDataCollector|null */
 	protected $dbDataCollector = null;
+	private int $lastConnectionCheck = 0;
 
 	/**
 	 * Initializes a new instance of the Connection class.
@@ -123,9 +125,12 @@ class Connection extends PrimaryReadReplicaConnection {
 	public function connect($connectionName = null) {
 		try {
 			if ($this->_conn) {
+				$this->reconnectIfNeeded();
 				/** @psalm-suppress InternalMethod */
 				return parent::connect();
 			}
+
+			$this->lastConnectionCheck = time();
 
 			// Only trigger the event logger for the initial connect call
 			$eventLogger = \OC::$server->get(IEventLogger::class);
@@ -617,5 +622,19 @@ class Connection extends PrimaryReadReplicaConnection {
 			$this->logger->debug('Switched to primary database', ['exception' => new \Exception()]);
 		}
 		return $result;
+	}
+
+	private function reconnectIfNeeded(): void {
+		if ($this->lastConnectionCheck + 30 >= time()) {
+			return;
+		}
+
+		try {
+			$this->_conn->query($this->getDriver()->getDatabasePlatform()->getDummySelectSQL());
+			$this->lastConnectionCheck = time();
+		} catch (ConnectionLost|\Exception $e) {
+			$this->logger->warning('Exception during connectivity check, closing and reconnecting', ['exception' => $e]);
+			$this->close();
+		}
 	}
 }
